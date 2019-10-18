ea52108f21e017d8d2f8246cea8c02e60a6fbc3a
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-10-18 18:12:15 +0200

    jsmath

--- static/js_numeric/mass.js
+++ static/js_numeric/mass.js
@@ -1,2 +1,22 @@
-// const {log10, ln} = require( '@stdlib/math/base/special' );
-// const pythonshell = require('python-shell')
+const {PythonShell} = require('python-shell')
+const path = require('path')
+const filename = "Y:\22pole_iontrap-exchange\Students\Aravindh\Measurements\CD+\masspec\30_08_19-9.mass".split('\\')
+const pythonPath = path.join(__dirname, "python3.7/python.exe")
+
+pycode = `
+from numpy import genfromtxt as openfile
+from pathlib import Path as pt
+import json
+massfile = pt("${filename}")
+mass, counts = openfile(massfile).T
+data = json.dumps({"x":list(mass), "y":list(counts)})
+
+print(data)
+`
+PythonShell.runString(pycode, { pythonPath: pythonPath }, function (err, data) {
+
+    if (err) throw err;
+    data = JSON.parse(data)
+    console.log(data.x.length);
+    
+  });
