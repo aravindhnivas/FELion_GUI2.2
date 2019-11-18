fd726163a39c38d64db080788233e2087b607ccd
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-11-18 22:54:33 +0100

    Update delete_fileLines.py

--- static/python_files/delete_fileLines.py
+++ static/python_files/delete_fileLines.py
@@ -1,7 +1,21 @@
-
 import os, sys
 from pathlib import Path as pt
-from FELion_definitions import delete_last_line
+
+def delete_last_line(filename):
+
+    print(f"File received: {filename}")
+    with open(filename, "r+", encoding = "utf-8") as file:
+
+        file.seek(0, os.SEEK_END)
+        pos = file.tell() - 2
+        while pos > 0 and file.read(1) != "\n":
+            pos -= 1
+            file.seek(pos, os.SEEK_SET)
+        if pos > 0:
+            file.seek(pos, os.SEEK_SET)
+            file.truncate()
+
+        print(f"Last line deleted")
 
 if __name__ == "__main__":
     args = sys.argv[1:][0].split(",")
