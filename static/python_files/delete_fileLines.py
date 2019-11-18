1ce71a431116f9d0756a59a7975351ccf01355f9
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-11-18 22:56:10 +0100

    Update delete_fileLines.py

--- static/python_files/delete_fileLines.py
+++ static/python_files/delete_fileLines.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     args = sys.argv[1:][0].split(",")
 
     filename = f"{args[0]}.expfit"
+    
     location = pt(args[1])
     if location.name is "DATA": datfile_location = location.parent/"EXPORT"
     else: datfile_location = location/"EXPORT"
