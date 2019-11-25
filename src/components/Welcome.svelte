a1b40fba74e917f2bd4664b9d4931f6cc9525859
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-11-25 22:16:18 +0100

    animate box container

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -1,27 +1,23 @@
 <script>
+	import AnimateBox from "./utils/AnimateBox.svelte"
   export let jq;
   console.log("Loading")
-  jq(document).ready(()=>{
-    jq("#Welcome").css("display", "block");
-  })
+  jq(document).ready(()=>jq("#Welcome").css("display", "block"))
 </script>
-
 <style>
+
   #Welcome {
     margin-top: 15% !important;
     display: none;
   }
-  .container {
-    border: 5px solid #fafafaa6;
-    padding: 2em;
-    box-shadow: 12px 12px 2px 1px #7a7a7a96;
-  }
+  
 </style>
 
 <section class="section animated fadeInDown" id="Welcome">
-  <div class="container">
-    
+
+  <AnimateBox>
     <h1 class="title">FELion Spectrum Analyser</h1>
     <h2 class="subtitle">To analyse FELIX data for FELion Instrument.</h2>
-  </div>
+  </AnimateBox>
+
 </section>
