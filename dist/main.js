dce2a690e11dd67dacc4e007e0125fb71437f434
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-12-21 00:07:42 +0100

    clean index.js layout

--- dist/main.js
+++ dist/main.js
@@ -83,4 +83,3 @@ electron.app.on("activate", function () {
     }
 });
 //# sourceMappingURL=main.js.map
-//# sourceMappingURL=main.js.map
--- dist/renderer.js
+++ dist/renderer.js
@@ -12110,7 +12110,6 @@ var Popper = function () {
 Popper.Utils = (typeof window !== 'undefined' ? window : global).PopperUtils;
 Popper.placements = placements;
 Popper.Defaults = Defaults;
-//# sourceMappingURL=popper.js.map
 
 /**!
 * tippy.js v4.3.5
@@ -14050,7 +14049,6 @@ function injectCSS(css) {
 }
 
 injectCSS(css);
-//# sourceMappingURL=index.all.js.map
 
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
@@ -25111,7 +25109,6 @@ const app = new App({
     target: document.body,
     props: { mainPages }
 });
-//# sourceMappingURL=renderer.js.map
 
 module.exports = app;
 //# sourceMappingURL=renderer.js.map
