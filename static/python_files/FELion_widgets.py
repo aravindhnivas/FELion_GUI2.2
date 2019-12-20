b616fa808c2c2c3e261049833199a4d187fea02b
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2019-12-20 23:01:44 +0100

    Update FELion_widgets.py

--- static/python_files/FELion_widgets.py
+++ static/python_files/FELion_widgets.py
@@ -3,9 +3,10 @@
 import os, sys, time, traceback
 from os.path import isdir, isfile
 from pathlib import Path as pt
+from io import StringIO
+import contextlib
 
-# import tkinter as tk
-
+# Tkinter
 from tkinter import Frame, IntVar, StringVar, BooleanVar, DoubleVar, Tk, filedialog, END, Text
 from tkinter.ttk import Button, Checkbutton, Label, Entry, Scale, Scrollbar, OptionMenu
 from tkinter.messagebox import showerror, showinfo, showwarning, askokcancel
@@ -13,16 +14,10 @@ from tkinter.messagebox import showerror, showinfo, showwarning, askokcancel
 # Matplotlib
 from matplotlib import style
 import matplotlib.pyplot as plt
-# import matplotlib
-
-# matplotlib.use("TkAgg")
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 from matplotlib.ticker import AutoMinorLocator
-from io import StringIO
-import contextlib
 
 @contextlib.contextmanager
 def stdoutIO(stdout=None):
@@ -33,10 +28,6 @@ def stdoutIO(stdout=None):
     yield stdout
     sys.stdout = old
 
-def clog(*args): print(args)
-def getchild(artist): return artist.get_children()
-############################################################################################################
-
 constants = {
     'relwidth': 0.3,
     'relheight': 0.04,
@@ -45,14 +36,16 @@ constants = {
 }
 
 def var_check(kw):
+    
     for i in constants:
-        if not i in kw:
-            kw[i] = constants[i]
+        if not i in kw: kw[i] = constants[i]
     return kw
+
 
 class FELion_Tk(Tk):
 
     def __init__(self, title="FELion GUI2", location=".", background="light grey", *args, **kwargs):
+
         Tk.__init__(self, *args, **kwargs)
 
         self.location = pt(location)
@@ -60,17 +53,18 @@ class FELion_Tk(Tk):
 
         Tk.wm_title(self, title)
         Tk.wm_geometry(self, "1000x600")
-
+        
         self.canvas_frame = Frame(self, bg='white')
         self.canvas_frame.place(relx=0, rely=0, relwidth=0.8, relheight=1)
 
         self.widget_frame = Frame(self, bg=background)
         self.widget_frame.bind("<Button-1>", lambda event: self.focus())
+
         self.widget_frame.place(relx=0.8, rely=0, relwidth=0.2, relheight=1)
 
     def Labels(self, txt, x, y, **kw):
-
         kw = var_check(kw)
+        
         self.widget_frame.txt = Label(self.widget_frame, text=txt)
         self.widget_frame.txt.place(
             relx=x, rely=y, anchor=kw['anchor'], relwidth=kw['relwidth'], relheight=kw['relheight'])
