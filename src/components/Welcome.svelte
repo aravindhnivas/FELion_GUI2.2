58fec42c2263c6114672d1bdb83ad578ea91ed61
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2020-01-08 21:16:42 +0100

    draggable color picker div integrated in welcome screen

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -1,6 +1,7 @@
 <script>
 
   import AnimateBox from "./utils/AnimateBox.svelte";
+  import ColorPicker from "./utils/ColorPicker.svelte";
   export let jq;
   let animation_welcome;
   console.log("Loading");
@@ -54,111 +55,10 @@
         easing: "easeOutExpo",
         delay: 1000
       });
-
-  
-    const pickr = Pickr.create({
-      el: '.color-picker',
-      theme: 'classic', // or 'monolith', or 'nano'
-
-      swatches: ["#46307d", "#38236b", "#4a3284", "#5b3ea2"],
-
-      components: {
-          preview: true,
-
-          opacity: true,
-          hue: true,
-          interaction: {
-
-              hex: true,
-              rgba: true,
-              hsla: true,
-              hsva: true,
-              cmyk: true,
-              input: true,
-              clear: false,
-              save: true
-          }
-      }
-
-    })
-
-
-    const addLight = (color, amount)=>{
-      let cc = parseInt(color,16) + amount;
-      let c = (cc > 255) ? 255 : (cc);
-      c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
-      return c;
-    }
-
-    const lighten = (color, amount)=> {
-      color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-      amount = parseInt((255*amount)/100);
-      return color = `#${addLight(color.substring(0,2), amount)}${addLight(color.substring(2,4), amount)}${addLight(color.substring(4,6), amount)}`;
-    }
-
-    const subtractLight = (color, amount)=>{
-      let cc = parseInt(color,16) - amount;
-      let c = (cc < 0) ? 0 : (cc);
-      c = (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`;
-      return c;
-    }
-
-    const darken = (color, amount) =>{
-      color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-      amount = parseInt((255*amount)/100);
-      return color = `#${subtractLight(color.substring(0,2), amount)}${subtractLight(color.substring(2,4), amount)}${subtractLight(color.substring(4,6), amount)}`;
-    }
-
-    const defaultColors = {body:"#46307d", header:"#38236b", navbar:"#4a3284", box:"#5b3ea2"}
-    
-    let colorChanged = false;
-    let newColor;
-    let backgroundDefaultColor = defaultColors.body;
-    let settingDefaultColor = false;
-
-    const changeStyleByClass = (className, color, amt=6, lightStyle=true) => {
-      let element = Array.from(document.getElementsByClassName(className))
-      let func;
-      lightStyle ? func = lighten : func = darken
-      element.forEach(file=>file.style.backgroundColor = func(color, amt))
-    }
-
-    const setBackgroundColor = (color) => {
-      document.documentElement.style.backgroundColor = color
-      document.body.style.backgroundColor = color
-      changeStyleByClass("navbar is-dark", color, 6, false)
-      changeStyleByClass("box", color)
-      changeStyleByClass("panel-heading", color, 6, false)
-      changeStyleByClass("row1", color)
-      document.getElementById("Navbar").style.backgroundColor = lighten(color, 2)
-
-    }
-
-    pickr.on('hide', instance => {
-        colorChanged ? setBackgroundColor(newColor) : setBackgroundColor(backgroundDefaultColor)
-
-    }).on('save', (color, instance) => {
-
-      console.log("Saving new color")
-      colorChanged = true
-      backgroundDefaultColor = color.toHEXA().toString()
-
-    }).on('change', (color, instance) => {
-
-      colorChanged = false
-      newColor = color.toHEXA().toString()
-
-      setBackgroundColor(newColor)
-
-    })
-
-    document.getElementsByClassName("pcr-button")[0].style.border = "solid 1px white"
-
   });
 </script>
 
 <style>
-
   #Welcome {
     margin-top: 15% !important;
     display: none;
@@ -200,9 +100,11 @@
   }
 </style>
 
+
 <section class="section" id="Welcome">
 
-  <div class="color-picker"></div>
+  <ColorPicker />
+
   <AnimateBox>
     <h1 class="ml5">
       <span class="text-wrapper">
